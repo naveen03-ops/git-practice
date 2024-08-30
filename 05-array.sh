16da8b9744220d5ac3f933a133643ef277825b65
Naveen <info@joindevops.com>
2024-08-30 16:29:56 +0530

    variables

--- 05-array.sh
+++ 05-array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Fruits=("APPLE" "KIWI" "ORANGE")
+FRUITS=("APPLE" "KIWI" "ORANGE")
 echo "FIRST fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
