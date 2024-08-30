128a0b87a2b80bc30afaa544e2606c1b8f508ffb
Naveen <info@joindevops.com>
2024-08-30 16:13:54 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your user name"
+read -s USERNAME
+echo "user name entered is: $USERNAME
+echo "please enter your password"
+read -s PASSWORD
+echo "password entered is: $PASSWORD
