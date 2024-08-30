a527fd446db9ee518415de55d5d7fd6443ba6f36
Naveen <info@joindevops.com>
2024-08-30 23:01:45 +0530

    functions

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,3 +5,5 @@ echo "Script name: $0"
 echo "current working directory: $PWD"
 echo "home directory of current user: $HOME"
 echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
