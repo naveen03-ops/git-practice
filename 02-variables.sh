cf4689ddaa1d4d7fa965fda0f5e39b73c187cc48
Naveen <info@joindevops.com>
2024-08-30 15:34:52 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+echo "$PESRON1::"Hi ${PERSON2} how ru doing"
+echo "$PESRON2::"Hi ${PERSON1} Iam fine.How r udoing"
+echo "$PESRON1::"Hi ${PERSON2} lerning devops"
