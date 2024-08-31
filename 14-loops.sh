da89d3a78cfd9aa3af19e9525d84691d749cbb9e
Naveen <info@joindevops.com>
2024-08-31 22:58:15 +0530

    shell

---
+++ 14-loops.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+#echo "USER ID is: $USERID"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "please run the script with root privileges"
+        exit 1
+    fi
+            }
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo -e "$2 is ..$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is ..$G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+for package in $@ # $@ refers to allarguments passed to it
+do  
+    dnf list installed $package 
+    if [ $? -ne 0 ]
+    then    
+        echo "$package is not installed,going to install it.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed ..nothing to do"
+    fi
+done
+
+
+# # if [ $USERID -ne 0 ]
+# # then
+# #     echo "please run this script with root privileges"
+# #     exit 1
+# # fi
+
+#  dnf list installed git    #just checking whether it installed or not
+
+# VALIDATE $? "Listing git"
+
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Git is not installed,going to install it.."
+#     dnf install git -y #here installing
+#     VALIDATE $? "Installing Git"
+
+# #     if [ $? -ne 0 ]
+# #     then
+# #     echo "Git installation is not success..check it"
+# #     exit 1
+# #     else "git installation is success"
+# # fi
+# else
+#     echo "Git is already installed,nothing to do"
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "mysql is not installed,going to install it.."
+#     dnf install mysql -y
+# #     
+#     VALIDATE $? "Installing Mysql"
+# else
+#     echo "mysql is already installed,nothing to do"
+# fi
