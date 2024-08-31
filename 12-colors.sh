4a85f6fed669aafa693fe701a1b75648aadb2c69
Naveen <info@joindevops.com>
2024-08-31 15:05:54 +0530

    functions

--- 12-colors.sh
+++ 12-colors.sh
@@ -26,11 +26,11 @@ VALIDATE(){
 
 CHECK-ROOT
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run this script with root privileges"
-    exit 1
-fi
+# if [ $USERID -ne 0 ]
+# then
+#     echo "please run this script with root privileges"
+#     exit 1
+# fi
 
 dnf list installed git
 
